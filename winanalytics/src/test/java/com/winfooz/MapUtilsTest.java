0355dfa1401cfaf09a64ff2c9ca22c4f2c02cb18
Mohamed Hamdan <mhamdan@winfooz.com>
2018-12-02 09:16:17 +0200

    Fix classes formating

--- winanalytics/src/test/java/com/winfooz/MapUtilsTest.java
+++ winanalytics/src/test/java/com/winfooz/MapUtilsTest.java
@@ -1,28 +1,24 @@
 package com.winfooz;
 
-import org.json.JSONObject;
+import java.util.HashMap;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-
-import java.util.HashMap;
 
 /**
  * Project: WinAnalytics2 Created: November 18, 2018
  *
  * @author Mohamed Hamdan
  */
-@Config(manifest = Config.NONE)
 @RunWith(RobolectricTestRunner.class)
 public class MapUtilsTest {
 
     @Test
-    public void test_toJsonObject() {
+    public void testToJsonObject() {
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("test", "test");
-        JSONObject object = MapUtils.toJsonObject(hashMap);
-        Assert.assertTrue(object.has("test"));
+
+        Assert.assertTrue(MapUtils.toJsonObject(hashMap).has("test"));
     }
 }
